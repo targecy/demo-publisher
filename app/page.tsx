cd238b59c5a10ee249bf7549dd77352da6c5dc01
Martin Grabina <mgrabina@itba.edu.ar>
2023-12-19 12:11:41 -0300

    fix

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import Image from "next/image";
 import { AdParams, Ad } from "@targecy/sdk";
 
 export default function Home() {
+  
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-center font-mono text-sm lg:flex">
